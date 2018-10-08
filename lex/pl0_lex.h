dd7311911944d69ac209e80cde46546d1b8a6073
PB16120100 <jxq96@mail.ustc.edu.cn>
2018-10-08 02:26:20 -0700

    modified the definition of struct lex

--- lex/pl0_lex.h
+++ lex/pl0_lex.h
@@ -1,7 +1,7 @@
 #ifndef __PL0_LEX_H__
 #define __PL0_LEX_H__
 
-
+#include "../common/common.h"
 #define NRW			11	/* Number of reserved words */
 #define NSYM		16	/* Number of symbols */
 #define MAX_NUM_LEN	9	/* Maximum length of a number, set to 9 to fit in range of C (-2147483648 to 2147483647) */
@@ -47,7 +47,6 @@ typedef enum _tPL0TokenType {
 
 extern const char * TOKEN_RESERVED_WORDS[NRW];
 extern const char * TOKEN_SYMBOLS[NSYM];
-
 typedef struct _tPL0Lex {
 	/* Parent return pointer */
 	struct _tPL0Compiler * compiler;
