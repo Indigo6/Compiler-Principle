8e7ce409f9f72c89ea979d6efe5bc3368a2a9e37
indigo <samariat@mail.ustc.edu.cn>
2018-10-20 15:07:50 +0800

    nothing

---
+++ cmake-build-debug/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/indigo/cpl/pl0compiler/test.c" "/home/indigo/cpl/pl0compiler/cmake-build-debug/CMakeFiles/test.dir/test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/indigo/cpl/pl0compiler/cmake-build-debug/lex/CMakeFiles/lex.dir/DependInfo.cmake"
+  "/home/indigo/cpl/pl0compiler/cmake-build-debug/common/CMakeFiles/common.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
